8d9cc7bb0af3706c3ef562ebf226992fc5db8114
Amaliya-mel <melsovna1712@yandex.ru>
2024-10-03 16:14:44 +0300

    Add files via upload

---
+++ lesson-77/main.js
@@ -0,0 +1,25 @@
+const sum = 45 + 90
+console.log(sum)
+
+const minus = 1365 - 456
+console.log(minus)
+
+const x = 345
+const y = 45
+const result = x * y / 2
+console.log(x * y / 2)
+
+
+console.log(9**2)
+
+console.log (75%6)
+
+const num = 567 
+console.log (-num)
+
+console.log('Amaliya ' + '20 y.a')
+
+
+
+
+
